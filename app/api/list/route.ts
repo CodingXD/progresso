e4384c19f87196811af6308bfdf0ae8e4b0be2af
Marshall Shelly <Marshallshelly97@gmail.com>
2023-05-26 22:02:26 +0530

    moved route to edge

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,6 +1,8 @@
 import { listItem } from "@/database";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export async function GET(request: Request) {
   const data = listItem();
   return NextResponse.json(data);
